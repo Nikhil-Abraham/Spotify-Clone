b30b8bd856907b5cf742343fb42a4800d7cbb86a
Nikhil Abraham <nikhil.abraham2001@gmail.com>
2021-07-29 12:42:42 +0530

    creating context api code

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import Player from "./Player";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -13,11 +19,18 @@ function App() {
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
     }
   }, []);
-  return (
-    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
-  );
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ spotify-clone/src/DataLayer.js
@@ -0,0 +1,11 @@
+import React, { createContext, userContext, useReducer } from "react";
+
+export const DataLayerContext = createContext();
+
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </DataLayerContext.Provider>
+);
+
+export const useDataLayerValue = () => useContext(DataLayerContext);
